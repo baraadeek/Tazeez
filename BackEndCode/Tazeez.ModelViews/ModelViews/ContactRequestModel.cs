c0fc004c1bda68b9c9173fa13c77abc0aab7c8be
baraade <bdeek@DESKTOP-50RTRBC>
2021-12-25 14:35:47 +0200

    change-db add data

--- BackEndCode/Tazeez.ModelViews/ModelViews/ContactRequestModel.cs
+++ BackEndCode/Tazeez.ModelViews/ModelViews/ContactRequestModel.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tazeez.ModelViews.ModelViews
 {
     public class ContactRequestModel
     {
         public int Id { get; set; }
+        
+        [Required]
         public string UserName { get; set; }
+        
         public string Email { get; set; }
+        
         public string PhoneNumber { get; set; }
+        
         public string Message { get; set; }
+        
         public DateTime CreatedDate { get; set; }
+        
         public DateTime UpdateDate { get; set; }
+        
         public bool Archived { get; set; }
     }
 }
