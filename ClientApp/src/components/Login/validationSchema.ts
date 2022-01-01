c385eca3c5f07c8862b8fefaced139f4712ca075
Yousef odeh <yousef.odeh.1998@gmail.com>
2022-01-01 15:22:10 +0200

    delete old code

--- ClientApp/src/components/Login/validationSchema.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-
-export const loginValidationSchema = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().min(10).max(32).required(),
-});
