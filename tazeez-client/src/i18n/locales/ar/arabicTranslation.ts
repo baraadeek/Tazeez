59a33ccef0698b657981806265ab8ef6090ec277
Yousef odeh <yousef.odeh.1998@gmail.com>
2022-01-12 19:39:14 +0200

    add Login

--- tazeez-client/src/i18n/locales/ar/arabicTranslation.ts
+++ tazeez-client/src/i18n/locales/ar/arabicTranslation.ts
@@ -10,6 +10,14 @@ import { namespaces } from "i18n/i18n.constants";
   [namespaces.pages.home]: {
     welcome: "مرحبا",
   },
+  [namespaces.pages.login]:{
+    signIn: "تسجيل دخول",
+    home: "الصفحة الرئيسية",
+    createAccount: "ألم تقم بحسابك بعد؟",
+    signUp: "سجل هنا",
+    enterEmail: "أدخل البريد الإلكتروني",
+    enterPassword: "أدخل الرقم السري"
+  }
 };
 
 export default arabicTranslation;
