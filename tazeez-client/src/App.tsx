c1de4ad2d81551a15f35b291cd26773c549b7880
AhmadHddad <AhmadHddad@outlook.com>
2022-01-14 22:37:50 +0200

    clean up app

--- tazeez-client/src/App.tsx
+++ tazeez-client/src/App.tsx
@@ -1,31 +1,19 @@
 import "./App.css";
 import { Route, Redirect, Switch } from "react-router-dom";
 // import routes from "./routes/routes";
-import React, { Fragment, Suspense, useEffect, useMemo, useState } from "react";
+import React, {  useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import { IRootReducer } from "./store/reducers/rootReducer";
 import { normalRoutes, authRoutes } from "routes/routes";
 
-// Material Dashboard 2 React components
-
-// Material Dashboard 2 React example components
-import Sidenav from "examples/Sidenav";
-import Configurator from "examples/Configurator";
-
-// @mui material components
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-
 // RTL plugins
 import rtlPlugin from "stylis-plugin-rtl";
-import { CacheProvider, EmotionCache } from "@emotion/react";
+import {  EmotionCache } from "@emotion/react";
 import createCache from "@emotion/cache";
 import { useMaterialUIController } from "context";
 
 // Material Dashboard 2 React themes
-import theme from "assets/theme";
 import { ROUTES_PATH_ENUM } from "common/constants/routesPathEnum";
-import Login from "views/login/Login";
 import HomeLayout from "views/layouts/HomeLayout/HomeLayout";
 import AdminLayout from "views/layouts/AdminLayout/AdminLayout";
 // import themeRTL from "assets/theme/theme-rtl";
@@ -39,14 +27,7 @@ function App() {
   const [rtlCache, setRtlCache] = useState<EmotionCache | null>(null);
 
   const {
-    miniSidenav,
     direction,
-    layout,
-    openConfigurator,
-    sidenavColor,
-    transparentSidenav,
-    whiteSidenav,
-    darkMode,
   } = controller;
 
   // Cache for the rtl
@@ -69,11 +50,8 @@ function App() {
     (state) => state.authReducer.token !== null
   ) as boolean;
 
-  if (true) {
-    // const routs = authRoutes.map((rout) => (
-    //   <Route key={rout.id} path={rout.path} exact component={rout.component} />
-    // ));
-    const mainPage = authRoutes.find((r) => r.isMain === true);
+  if (isAuthenticated) {
+
 
     return <AdminLayout routes={authRoutes}>{"Ahmad"}</AdminLayout>;
   } else {
