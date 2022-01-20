99bbbec2e6ab190c3bebadfd59e1af2973c67ca6
AhmadHddad <AhmadHddad@outlook.com>
2022-01-20 14:17:54 +0200

    using AppDirectionEnum

--- tazeez-client/src/common/hooks/appHooks.ts
+++ tazeez-client/src/common/hooks/appHooks.ts
@@ -1,11 +1,11 @@
+import { AppDirectionEnum } from "common/constants/directionEnum";
 import { useSelector } from "react-redux";
-import { IAppDirection } from "store/reducers/appReducer";
 import { IRootReducer } from "store/reducers/rootReducer";
 
 export function useIsRtl() {
   const direction = useSelector<IRootReducer>(
     (state) => state.app.direction
-  ) as IAppDirection;
+  ) as AppDirectionEnum;
 
   return direction === "rtl";
 }
