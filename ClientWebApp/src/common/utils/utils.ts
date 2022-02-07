e43131561e547d3cfbed824c31200acee9da414b
AhmadHddad <AhmadHddad@outlook.com>
2022-02-07 19:34:58 +0200

    adding uuidvv4 util

--- ClientWebApp/src/common/utils/utils.ts
+++ ClientWebApp/src/common/utils/utils.ts
@@ -127,5 +127,15 @@ export function conditionalRender(render: boolean, component: any) {
   return render ? component : null;
 }
 
-export const getKeyValue = <T, K extends keyof T>(obj: T, key: K): T[K] => obj[key];
+export const getKeyValue = <T, K extends keyof T>(obj: T, key: K): T[K] =>
+  obj[key];
 
+export function uuidv4() {
+  //@ts-ignore
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
