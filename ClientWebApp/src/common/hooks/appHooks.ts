44001dd3a4b1ab94bcf1e5aa4f220fb301fdb1c9
AhmadHddad <AhmadHddad@outlook.com>
2022-02-11 18:41:54 +0200

    adding new hooks

--- ClientWebApp/src/common/hooks/appHooks.ts
+++ ClientWebApp/src/common/hooks/appHooks.ts
@@ -1,4 +1,12 @@
 import { AppDirectionEnum } from "common/constants/directionEnum";
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { useSelector } from "react-redux";
 import { IRootReducer } from "store/reducers/rootReducer";
 
@@ -8,4 +16,26 @@ export function useIsRtl() {
   ) as AppDirectionEnum;
 
   return direction === "rtl";
+}
+
+export function useMountedState<S>(
+  initialState: S | (() => S)
+): [S, Dispatch<SetStateAction<S>>] {
+  const isMounted = useRef(true);
+  const [state, setState] = useState<S>(initialState);
+
+  useEffect(
+    () => () => {
+      isMounted.current = false;
+    },
+    []
+  );
+
+  const onSetState = useCallback(function (state) {
+    if (isMounted.current) {
+      setState(state);
+    }
+  }, []);
+
+  return [state, onSetState];
 }
