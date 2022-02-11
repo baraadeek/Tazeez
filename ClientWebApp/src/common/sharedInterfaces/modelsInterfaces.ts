6ff689a2117b7441ae556c81b8de3c43f4f073ba
AhmadHddad <AhmadHddad@outlook.com>
2022-02-11 18:43:14 +0200

    adding modelsInterfaces

---
+++ ClientWebApp/src/common/sharedInterfaces/modelsInterfaces.ts
@@ -0,0 +1,29 @@
+export type IUser = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  image: string;
+  gender?: any;
+  email: string;
+};
+
+export type IQuestion = {
+  questionId: number;
+  commentsCount: number;
+  attachmentsCount: number;
+  questionnaireId: number;
+  displayOrder: number;
+  questionType: string | number;
+  questionText: string;
+  answer: string | null;
+  additionalAnswer: string;
+  isReadOnly: boolean;
+  isOptional: boolean;
+  isQuestionAnswered: boolean;
+  status: 0 | 1 | 2;
+  questionnaireGroupTemplateQuestionName: string;
+  questionnaireGroupTemplateQuestionId: number;
+  assignedUserId: number;
+  answeredByUserId: number | null;
+  answerChoices: [];
+};
